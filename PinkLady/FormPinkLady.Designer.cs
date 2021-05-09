0720020f670d3d1bd3f9a4e6a2e8d471f4577899
pink-tan <pink.tan.tank@gmail.com>
2021-05-09 21:51:14 +0900

    初期登録

---
+++ PinkLady/FormPinkLady.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace PinkLady
+{
+    partial class FormPinkLady
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPinkLady));
+            this.cmbStage = new System.Windows.Forms.ComboBox();
+            this.picboxCur = new System.Windows.Forms.PictureBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.TopDownToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.BottomUp_ClickToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.picboxNext = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxCur)).BeginInit();
+            this.contextMenuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picboxNext)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.cmbStage.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbStage.FormattingEnabled = true;
+            this.cmbStage.Items.AddRange(new object[] {
+            "1面",
+            "2面",
+            "3面",
+            "4面",
+            "5面",
+            "6面"});
+            this.cmbStage.Location = new System.Drawing.Point(9, 12);
+            this.cmbStage.Name = "comboBox1";
+            this.cmbStage.Size = new System.Drawing.Size(130, 20);
+            this.cmbStage.TabIndex = 0;
+            this.cmbStage.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.picboxCur.Location = new System.Drawing.Point(9, 38);
+            this.picboxCur.Name = "pictureBox1";
+            this.picboxCur.Size = new System.Drawing.Size(130, 173);
+            this.picboxCur.TabIndex = 1;
+            this.picboxCur.TabStop = false;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.TopDownToolStripMenuItem,
+            this.BottomUp_ClickToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(128, 48);
+            // 
+            // 上から下へToolStripMenuItem
+            // 
+            this.TopDownToolStripMenuItem.Name = "TopDownToolStripMenuItem";
+            this.TopDownToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.TopDownToolStripMenuItem.Text = "上から下へ";
+            this.TopDownToolStripMenuItem.Click += new System.EventHandler(this.TopDown_Click);
+            // 
+            // 下から上へToolStripMenuItem
+            // 
+            this.BottomUp_ClickToolStripMenuItem.Name = "BottomUpToolStripMenuItem";
+            this.BottomUp_ClickToolStripMenuItem.Size = new System.Drawing.Size(127, 22);
+            this.BottomUp_ClickToolStripMenuItem.Text = "下から上へ";
+            this.BottomUp_ClickToolStripMenuItem.Click += new System.EventHandler(this.BottomUp_Click);
+            // 
+            // pictureBox2
+            // 
+            this.picboxNext.Location = new System.Drawing.Point(9, 217);
+            this.picboxNext.Name = "pictureBox2";
+            this.picboxNext.Size = new System.Drawing.Size(130, 16);
+            this.picboxNext.TabIndex = 2;
+            this.picboxNext.TabStop = false;
+            // 
+            // FormPinkLady
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.ClientSize = new System.Drawing.Size(148, 236);
+            this.ContextMenuStrip = this.contextMenuStrip1;
+            this.Controls.Add(this.picboxNext);
+            this.Controls.Add(this.picboxCur);
+            this.Controls.Add(this.cmbStage);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormPinkLady";
+            this.Text = "PinkLady";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxCur)).EndInit();
+            this.contextMenuStrip1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.picboxNext)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbStage;
+        private System.Windows.Forms.PictureBox picboxCur;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem TopDownToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem BottomUp_ClickToolStripMenuItem;
+        private System.Windows.Forms.PictureBox picboxNext;
+    }
+}
+
---
+++ PinkLady/FormPinkLady.cs
@@ -0,0 +1,351 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace PinkLady
+{
+    /// <summary>
+    /// フォームクラス
+    /// </summary>
+    public partial class FormPinkLady : Form
+    {
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public FormPinkLady()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 上から下フラグ
+        /// </summary>
+        private bool TopDown = true;
+        /// <summary>
+        /// フォントクラスのインスタンス使いまわし用クラス
+        /// </summary>
+        private readonly FontManager fontManager = new FontManager();
+
+        /// <summary>
+        /// フォームロードイベントハンドラ
+        /// </summary>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 面選択状態初期化
+            cmbStage.Text = "1面";
+            // 描画制御
+            Draw();
+        }
+
+        /// <summary>
+        /// 面選択状態変化イベントハンドラ
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ComboBoxSelectedIndexChanged(object sender, EventArgs e)
+        {
+            // 描画制御
+            Draw();
+        }
+
+        /// <summary>
+        /// 描画制御
+        /// </summary>
+        private void Draw()
+        {
+            // リソーステープル
+            string[] DATA = 
+            {
+                PinkLady.Properties.Resources._1,
+                PinkLady.Properties.Resources._2,
+                PinkLady.Properties.Resources._3,
+                PinkLady.Properties.Resources._4,
+                PinkLady.Properties.Resources._5,
+                PinkLady.Properties.Resources._6,
+                string.Empty
+            };
+
+            // 面選択状態取得
+            int index = cmbStage.SelectedIndex;
+            // 面選択状態が有効な範囲の場合
+            if ((index >= 0) && (index < 6))
+            {
+                // 現在の面についてリソース読み込み
+                string data = DATA[index];
+                // 改行コードでsplit
+                string[] splited = data.Replace("\r\n", "\n").Split('\n');
+                /// 上から下の場合
+                if (TopDown)
+                {
+                    // PictureBoxの配置
+                    picboxCur.Top =  38;
+                    picboxNext.Top = 214;
+                }
+                /// 下から上の場合
+                else
+                {
+                    // PictureBoxの配置
+                    picboxCur.Top = 58;
+                    picboxNext.Top = 38;
+                    // リソースの反転
+                    Array.Reverse(splited);
+                }
+                // ファイル単位の描画
+                Draw(picboxCur, splited, true, 3, false);
+
+
+                // 次の面についてリソース読み込み
+                string nextdata = DATA[index + 1];
+                // 改行コードでsplitした先頭のみを予告
+                string[] nextsplited = new string[1];
+                nextsplited[0] = nextdata.Replace("\r\n", "\n").Split('\n')[0];
+                // ファイル単位の描画
+                Draw(picboxNext, nextsplited, false, 3, true);
+            }
+        }
+
+        /// <summary>
+        /// ファイル単位の描画
+        /// </summary>
+        /// <param name="pictureBox">描画対象PictureBox</param>
+        /// <param name="data">ファイルの内容</param>
+        /// <param name="big">フォントサイズ大きいフラグ</param>
+        /// <param name="startPosY">描画開始Y座標</param>
+        /// <param name="nextStage">次の面フラグ</param>
+        private void Draw(PictureBox pictureBox, string[] data, bool big, int startPosY, bool nextStage)
+        {
+            // Graphicsの取得
+            pictureBox.Image = new Bitmap(pictureBox.Width, pictureBox.Height);
+            Graphics graphics = Graphics.FromImage(pictureBox.Image);
+            // 描画対象PictureBoxの初期化
+            using (Brush brush = new SolidBrush(Color.FromArgb(255, 240, 240)))
+            {
+                graphics.FillRectangle(brush, graphics.VisibleClipBounds);
+            }
+
+            // 基本フォント取得
+            Font defaultFont = fontManager.GetFont(big);
+
+            // 1行ずつ処理
+            int y = startPosY;
+            for (int i = 0; i < data.Length; i++)
+            {
+                // foreach使いたいけど我慢
+                string line = data[i];
+                // 空行の場合
+                if (line.Length == 0)
+                {
+                    // 読み飛ばし
+                    continue;
+                }
+                // --の場合
+                else if (line == "--")
+                {
+                    // 隙間を作る
+                    y += (int)defaultFont.Size / 2 + 2;
+                }
+                // 上記以外の場合
+                else
+                {
+                    // | でsplit
+                    string[] ufos = line.Split('|');
+
+                    // 1UFOずつ処理
+                    int x = ((int)graphics.VisibleClipBounds.Width - (ufos.Length * ((int)(defaultFont.Size * 1.3)))) / 2;
+                    for (int j = 0; j < ufos.Length; j++)
+                    {
+                        // foreach使いたいけど我慢
+                        string ufo = ufos[j];
+                        // UFO単位の描画
+                        Draw(graphics, ufo, big, x, y);
+                        // 右に移動
+                        x += (int)defaultFont.Size + 3;
+                    }
+
+                    // 次の面についての場合
+                    if (nextStage)
+                    {
+                        // "Next"描画
+                        graphics.DrawString("Next", defaultFont, Brushes.Gray, 0, y);
+                    }
+
+                    // 下に移動
+                    y += (int)defaultFont.Size + 4;
+                }
+            }
+            // Graphicsの破棄
+            graphics.Dispose();
+        }
+
+        /// <summary>
+        /// UFO単位の描画
+        /// </summary>
+        /// <param name="graphics">Graphics</param>
+        /// <param name="ufo">UFO情報</param>
+        /// <param name="big">フォントサイズ大きいフラグ</param>
+        /// <param name="x">X座標</param>
+        /// <param name="y">Y座標</param>
+        private void Draw(Graphics graphics, string ufo, bool big, int x, int y)
+        {
+            Font font;
+            Brush color = Brushes.BlueViolet;
+
+            // 文字の色
+            switch (ufo)
+            {
+                case "赤":
+                case "R赤":
+                    color = Brushes.Red;
+                    break;
+                case "緑":
+                case "R緑":
+                    color = Brushes.Green;
+                    break;
+                case "青":
+                case "R青":
+                    color = Brushes.Blue;
+                    break;
+                default:
+                    break;
+            }
+
+            // 文字とフォント
+            switch (ufo)
+            {
+                case "R赤":
+                case "R緑":
+                case "R青":
+                    ufo = "虹";
+                    font = fontManager.GetFont(big, false);
+                    break;
+                default:
+                    font = fontManager.GetFont(big, true);
+                    break;
+            }
+
+            // 描画
+            graphics.DrawString(ufo, font, color, x, y);
+        }
+
+        /// <summary>
+        /// 上から下へクリック時動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TopDown_Click(object sender, EventArgs e)
+        {
+            // 上から下へフラグを立てる
+            TopDown = true;
+            // 描画制御
+            Draw();
+        }
+
+        /// <summary>
+        /// 下から上へクリック時動作
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BottomUp_Click(object sender, EventArgs e)
+        {
+            // 上から下へフラグを落とす
+            TopDown = false;
+            // 描画制御
+            Draw();
+        }
+    }
+
+    /// <summary>
+    /// フォントクラスのインスタンス使いまわし用クラス
+    /// </summary>
+    public class FontManager
+    {
+        /// <summary>
+        /// フォント名
+        /// </summary>
+        private const string FONTNAME = "ＭＳ ゴシック";
+        /// <summary>
+        /// 大きくて太い
+        /// </summary>
+        public Font BigBold { get; set; }
+        /// <summary>
+        /// 大きくて太くない
+        /// </summary>
+        public Font Big { get; set; }
+        /// <summary>
+        /// 大きくなくて太い
+        /// </summary>
+        public Font SmallBold { get; set; }
+        /// <summary>
+        /// 大きくなくて太くない
+        /// </summary>
+        public Font Small { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public FontManager()
+        {
+            // 初期化
+            BigBold     = new Font(FONTNAME, 12, FontStyle.Bold);
+            Big         = new Font(FONTNAME, 12);
+            SmallBold   = new Font(FONTNAME, 9, FontStyle.Bold);
+            Small       = new Font(FONTNAME, 9);
+        }
+        /// <summary>
+        /// ファイナライザ
+        /// </summary>
+        ~FontManager()
+        {
+            // 解放
+            BigBold.Dispose();
+            Big.Dispose();
+            SmallBold.Dispose();
+            Small.Dispose();
+        }
+
+        /// <summary>
+        /// フォントクラスのインスタンス取得
+        /// </summary>
+        /// <param name="big">大きい</param>
+        /// <returns>フォントクラスのインスタンス</returns>
+        public Font GetFont(bool big)
+        {
+            return GetFont(big, false);
+        }
+
+        /// <summary>
+        /// フォントクラスのインスタンス取得
+        /// </summary>
+        /// <param name="big">大きい</param>
+        /// <param name="bold">太い</param>
+        /// <returns>フォントクラスのインスタンス</returns>
+        public Font GetFont(bool big, bool bold)
+        {
+            Font font;
+            if (big)
+            {
+                if (bold)
+                {
+                    font = BigBold;
+                }
+                else
+                {
+                    font = Big;
+                }
+            }
+            else
+            {
+                if (bold)
+                {
+                    font = SmallBold;
+                }
+                else
+                {
+                    font = Small;
+                }
+            }
+
+            return font;
+        }
+    }
+}
